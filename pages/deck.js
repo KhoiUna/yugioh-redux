e1fceca1a7d9d2d35447a971e45982fe7bbf9696
KhoiNguyen <thorwaitson@gmail.com>
2021-05-06 11:42:05 -0500

    add layout to app

--- pages/deck.js
+++ pages/deck.js
@@ -1,40 +1,5 @@
-import Head from "next/head";
-import styles from "../styles/index.module.css";
-import Link from "next/link";
+import Layout from "../containers/layout";
 
 export default function Deck() {
-  return (
-    <>
-      <Head>
-        <title>Yugioh Picks | My Deck</title>
-      </Head>
-
-      <header className={styles.header}>
-        <Link href="/">
-          <h1 style={{ fontSize: "1.8rem", margin: "0 1%", cursor: "pointer" }}>
-            Yugioh Picks
-          </h1>
-        </Link>
-        <Link href="/">
-          <span className={styles.span}>Go Back</span>
-        </Link>
-      </header>
-
-      <main>
-        <div></div>
-      </main>
-
-      <footer className={styles.footer}>
-        &copy; {new Date().getFullYear()} A product of{" "}
-        <a
-          href="https://bit.ly/khoiuna_links"
-          className={styles.link}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Khoi Nguyen
-        </a>
-      </footer>
-    </>
-  );
+  return <Layout page="deck"></Layout>;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Head from "next/head";
 import { useEffect, useState, Fragment } from "react";
 import Cards from "../utils/Cards";
 import Card from "../components/Card";
 import styles from "../styles/index.module.css";
-import Link from "next/link";
+import Layout from "../containers/layout";
 
 export default function Home() {
   const [cardsArray, setCardsArray] = useState(null);
@@ -11,71 +10,40 @@ export default function Home() {
     Cards.fetchAllCards().then((res) => setCardsArray(res));
   }, []);
 
-  const deckNum = null;
-
   return (
-    <>
-      <Head>
-        <title>Yugioh Picks</title>
-      </Head>
+    <Layout page="home">
+      <div style={{ margin: "3% 4%" }}>
+        <form>
+          <label htmlFor="card" hidden>
+            Card:
+          </label>
+          <input
+            id="card"
+            type="search"
+            className={styles.search_bar}
+            placeholder="Search your card here"
+          />
+        </form>
+      </div>
 
-      <header className={styles.header}>
-        <Link href="/">
-          <h1 style={{ fontSize: "1.8rem", margin: "0 1%", cursor: "pointer" }}>
-            Yugioh Picks
-          </h1>
-        </Link>
-        <Link href="/deck">
-          <span className={styles.span}>My deck ( {deckNum || 0} )</span>
-        </Link>
-      </header>
+      <div className={styles.flex_container}>
+        {cardsArray &&
+          cardsArray.slice(0, 21).map((info, index) => (
+            <Fragment key={index}>
+              <Card
+                imageURL={info.card_images[0].image_url}
+                cardName={info.name}
+              />
+            </Fragment>
+          ))}
+        {!cardsArray && (
+          <p style={{ fontWeight: "bold", fontSize: "1rem", margin: "5%" }}>
+            Loading...
+          </p>
+        )}
+      </div>
 
-      <main>
-        <div style={{ margin: "3% 4%" }}>
-          <form>
-            <label htmlFor="card" hidden>
-              Card:
-            </label>
-            <input
-              id="card"
-              type="search"
-              className={styles.search_bar}
-              placeholder="Search your card here"
-            />
-          </form>
-        </div>
-
-        <div className={styles.flex_container}>
-          {cardsArray &&
-            cardsArray.slice(0, 21).map((info, index) => (
-              <Fragment key={index}>
-                <Card
-                  imageURL={info.card_images[0].image_url}
-                  cardName={info.name}
-                />
-              </Fragment>
-            ))}
-          {!cardsArray && (
-            <p style={{ fontWeight: "bold", fontSize: "1rem", margin: "5%" }}>
-              Loading...
-            </p>
-          )}
-        </div>
-
-        <div className={styles.pagination}>1 2 3...</div>
-      </main>
-
-      <footer className={styles.footer}>
-        &copy; {new Date().getFullYear()} A product of{" "}
-        <a
-          href="https://bit.ly/khoiuna_links"
-          className={styles.link}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Khoi Nguyen
-        </a>
-      </footer>
-    </>
+      <div className={styles.pagination}>1 2 3...</div>
+    </Layout>
   );
 }
