e1cb6b44b53a6c0a68a731042b01a8598a789e05
KhoiNguyen <thorwaitson@gmail.com>
2021-05-06 13:44:04 -0500

    update pagination

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,9 +1,22 @@
 import styles from "../styles/index.module.css";
+import cx from "classnames";
 
 export default function Pagination({ pageQuery }) {
   return (
     <>
-      <div className={styles.pagination}>{"<< < 1 2 3... > >>"}</div>
+      <div className={styles.pagination_container}>
+        <span className={styles.pagination_button}>{"<<"}</span>
+        <span className={styles.pagination_button}>{"<"}</span>
+        <span className={styles.pagination_button}>
+          {pageQuery !== 1 && pageQuery}
+        </span>
+        <span className={cx(styles.pagination_button, styles.pagination_main)}>
+          {pageQuery}
+        </span>
+        <span className={styles.pagination_button}>{pageQuery + 1}</span>
+        <span className={styles.pagination_button}>{">"}</span>
+        <span className={styles.pagination_button}>{">>"}</span>
+      </div>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
         )}
       </div>
 
-      <Pagination/>
+      <Pagination pageQuery={1} />
     </Layout>
   );
 }
