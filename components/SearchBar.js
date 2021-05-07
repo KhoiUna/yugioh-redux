bd878f186aa46b018c2c90b0da1683d2a0388b10
KhoiNguyen <thorwaitson@gmail.com>
2021-05-07 03:14:53 -0500

    update SearchBar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,9 +1,17 @@
+import { useDispatch } from "react-redux";
+import { searchCardsAsync } from "../features/cardsSlice";
 import styles from "../styles/index.module.css";
 
 export default function SearchBar() {
+  const dispatch = useDispatch();
+  const handleChange = ({ target }) => {
+    if (target.value.length > 1) dispatch(searchCardsAsync(target.value));
+    return;
+  };
+
   return (
     <div style={{ margin: "3% 4%" }}>
-      <form>
+      <form onSubmit={(e) => e.preventDefault()}>
         <label htmlFor="card" hidden>
           Card:
         </label>
@@ -12,6 +20,8 @@ export default function SearchBar() {
           type="search"
           className={styles.search_bar}
           placeholder="Search your card here"
+          onChange={handleChange}
+          autoComplete="off"
         />
       </form>
     </div>
