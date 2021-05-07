b3bc64cadedd5ce8233524b3de68c2e941e35236
KhoiNguyen <thorwaitson@gmail.com>
2021-05-07 01:43:02 -0500

    update deck page UI

--- pages/deck.js
+++ pages/deck.js
@@ -1,15 +1,26 @@
 import { useSelector } from "react-redux";
 import Layout from "../containers/layout";
-import { selectDeck } from "../features/cardsSlice";
+import { selectDeck, selectDeckNum } from "../features/cardsSlice";
 import { Fragment } from "react";
 import Card from "../components/Card";
 import styles from "../styles/index.module.css";
 
 export default function Deck() {
   const deckArray = useSelector(selectDeck);
+  const deckNum = useSelector(selectDeckNum);
 
   return (
     <Layout page="deck">
+      <div className={styles.total_card}>
+        <h3>
+          <span
+            style={{ textDecoration: "3px underline", marginRight: "0.8rem" }}
+          >
+            Total Cards:
+          </span>
+          {deckNum}
+        </h3>
+      </div>
       <div className={styles.flex_container}>
         {deckArray.map((info, index) => (
           <Fragment key={index}>
