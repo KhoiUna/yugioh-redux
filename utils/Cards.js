8504655ff07273e4af51fc539ef0d74d9a2f8254
KhoiNguyen <thorwaitson@gmail.com>
2021-05-07 03:13:09 -0500

    update Cards util

--- utils/Cards.js
+++ utils/Cards.js
@@ -1,11 +1,11 @@
 export default class Cards {
-  static async fetchAllCards() {
+  static async fetchAllCards(limit) {
     const res = await (
       await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php")
     ).json();
 
-    const cardsArray = res.data;
-    return cardsArray;
+    const cardsArray = res.data.slice(limit * 20 - 20, limit * 20);
+    return { cardsArray, totalCardLength: res.data.length };
   }
 
   static async fetchCardsByName(cardName) {
